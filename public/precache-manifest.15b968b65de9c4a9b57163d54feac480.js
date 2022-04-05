3f7b707c43c618aedd9eb698959c4d1d0d683ecb
jaider012 <jaiderandres901@hotmail.com>
2022-04-04 22:11:02 -0500

    v1.1 de calendarioJP

---
+++ public/precache-manifest.15b968b65de9c4a9b57163d54feac480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591d6c3dc69406f421f370413c27a764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "9c747cabbc3c0c69b95d",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/js/2.bd64b146.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd64b146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c747cabbc3c0c69b95d",
+    "url": "/static/js/main.d610bf51.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
